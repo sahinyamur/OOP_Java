8adde8c0aaf48c7adf97cc10ac942d9247b87294
Yağmur Şahin <88347944+sahinyamur@users.noreply.github.com>
2023-06-14 22:37:06 +0300

    Add files via upload

---
+++ Car.java
@@ -0,0 +1,54 @@
+import java.sql.SQLOutput;
+
+public class Car {
+    private String color;
+    private int doors;
+    private int tyres;
+    private String motor;
+    private String model;
+
+    public void setModel(String model) {
+
+        this.model= model;
+    }
+    public String getModel() {
+        return this.model;
+    }
+
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public int getDoors() {
+        return doors;
+    }
+
+    public void setDoors(int doors) {
+     if (kapılar < 0){
+         System.out.println("Kapı sayısı O'dan kücük olamaz");
+     } else {
+         this.kapılar = kapilar;
+     }
+    }
+
+    public int getTyres() {
+        return tyres;
+    }
+
+    public void setTyres(int tyres) {
+        this.tyres = tyres;
+    }
+
+    public String getMotor() {
+        return motor;
+    }
+
+    public void setMotor(String motor) {
+        this.motor = motor;
+    }
+}
---
+++ Test.java
@@ -0,0 +1,18 @@
+public class Test {
+    public static void main (String[] args){
+
+       /* Car car1 = new Car(); //got a car obj
+
+        car1.setModel("Renault") ; {
+        System.out.println("Arabanin modeli  " + car1.getModel());
+        }*/
+
+     /*  Car car2;
+       car2.setDoors(10);*/
+      Car car3 = null;
+      car3.setModel("Renault"); 
+
+
+
+    }
+}
